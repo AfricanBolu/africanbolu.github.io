3c55bb3f2ca18910df911c0222084f700c30e04a
Bolu <87003907+AfricanBolu@users.noreply.github.com>
2026-01-11 15:32:53 -0600

    fixed css files for better styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,11 @@ const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Bolu</h1>
         <p className={styles.description}>I'm a fullstack developer, who likes to build things with code.</p>
-        <a href="mailto:oboluodianosen@gmail.com" className={styles.contactBtn}>
+        <a 
+          href="mailto:oboluodianosen@gmail.com" 
+          className={styles.contactBtn}
+          aria-label='Send email to Bolu'
+        >
           Contact Me
         </a>
       </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,12 +20,17 @@ const NavBar = () => {
         BOLU
       </a>
       <div className={styles.links}>
-        <img
+        <button
+          onClick={() => setIsOpen(!isOpen)}
           className={styles.linkBtn}
+          aria-label={isOpen ? 'Close menu' : 'Open menu'}
+          aria-expanded={isOpen}
+        >
+          <img
           src={isOpen ? closeIcon : menuIcon}
           alt="menu-button"
-          onClick={() => setIsOpen(!isOpen)}
         />
+        </button>
         <ul className={`${styles.linklist} ${isOpen && styles.active}`} onClick={() => setIsOpen(false)}>
           <li>
             <a href="#about">About</a>
